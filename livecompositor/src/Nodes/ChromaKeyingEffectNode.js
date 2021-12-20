0670f98ac9127bcc345db4c1465b2ab206947378
JoJo <Georgios.Padopoulos@outlook.de>
2021-12-20 15:37:00 +0100

    added chromakeying presets

--- livecompositor/src/Nodes/ChromaKeyingEffectNode.js
+++ livecompositor/src/Nodes/ChromaKeyingEffectNode.js
@@ -1,6 +1,10 @@
 export function ChromaKeyingEffectNode() {
   this.addInput("In", "array");
   this.addOutput("Out", "array");
+  //Widgets
+  this.chroma = this.addWidget("combo", "Chroma", "green", {
+    values: ["red", "green", "blue", "white", "black"],
+  });
 }
 
 ChromaKeyingEffectNode.title = "ChromaKeyingEffect";
@@ -21,10 +25,44 @@ ChromaKeyingEffectNode.prototype.onExecute = function () {
     const green = pixelArray.data[i + 1];
     const blue = pixelArray.data[i + 2];
     //preset black
-    if (green < 60 && red < 60 && blue < 60) {
-      pixelArray.data[i + 3] = 0;
-    } else if (green > 195 && red > 195 && blue > 195) {
-      pixelArray.data[i + 3] = 0;
+    switch (this.chroma.value) {
+      case "green":
+        if (red <= 45 && green >= 100 && blue <= 45 && Math.abs(red - blue) <= 30) {
+          pixelArray.data[i + 3] = 0;
+        }
+        break;
+      case "red":
+        if (red >= 150 && green <= 30 && blue <= 30 && Math.abs(green - blue) <= 20) {
+          pixelArray.data[i + 3] = 0;
+        }
+        break;
+      case "blue":
+        if (red <= 45 && green <= 45 && blue >= 125 && Math.abs(red - green) <= 25) {
+          pixelArray.data[i + 3] = 0;
+        }
+        break;
+      case "black":
+        if (
+          red <= 40 &&
+          green <= 40 &&
+          blue <= 40 &&
+          Math.abs(red - green) <= 5 &&
+          Math.abs(red - blue) <= 5
+        ) {
+          pixelArray.data[i + 3] = 0;
+        }
+        break;
+      case "white":
+        if (
+          red >= 210 &&
+          green <= 210 &&
+          blue <= 210 &&
+          Math.abs(red - green) <= 5 &&
+          Math.abs(red - blue) <= 5
+        ) {
+          pixelArray.data[i + 3] = 0;
+        }
+        break;
     }
   }
   this.setOutputData(0, pixelArray);
