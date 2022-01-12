997f6c564dc3208c2890d541e9e7f0860d02a979
programmdeveloper <timo.schnorz@student.htw-berlin.de>
2022-01-13 00:38:01 +0100

    Added volume, speed an checkbox for active to externalAudio
    Actually the input slider of speed doesn't adapt to the value

--- livecompositor/src/Components/AudioControl.js
+++ livecompositor/src/Components/AudioControl.js
@@ -3,19 +3,14 @@ import audioMixingInstance from '../AudioMixingController'
 
 const AudioControl = () => {
 
-    const adaptSpeed = () => {
-        console.log("Speed");
-        // check if > or < 0
-        // ==0 ?
-        // use audio.playback
-    }
-
     return (
         <div>
             <button id="playButton" onClick={() => audioMixingInstance.playExternal()}>Play</button>
             <button id="pauseButton" onClick={() => audioMixingInstance.pauseExternal()}>Pause</button>
-            <input id="volume" type="range" onChange={() => audioMixingInstance.adaptVolume(document.getElementById('volume').value)}/>
-            <input id="speed" type="range" onChange={adaptSpeed}/>
+            <input id="volume" type="range" min="0" max="2" step="0.01" onChange={() => audioMixingInstance.adaptVolume(document.getElementById('volume').value)}/>
+            <input value="1" id="speed" type="range" min="0.5" max="2" step="0.01" onChange={() => audioMixingInstance.adaptSpeed(document.getElementById('speed').value)}/>
+            <input type="checkbox" id="externalAudio" onChange={() => audioMixingInstance.externalActive()}/>
+            <label htmlFor="externalAudio">external audio active</label>
         </div>
     )
 }
