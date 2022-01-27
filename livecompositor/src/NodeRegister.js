e202789aff7a26b06bbe672bdf605169289c5a91
JoJo <Georgios.Padopoulos@outlook.de>
2022-01-27 14:28:17 +0100

    deleted circle and blur effect

--- livecompositor/src/NodeRegister.js
+++ livecompositor/src/NodeRegister.js
@@ -2,7 +2,6 @@
 import { OutputNode } from "./Nodes/OutputNode";
 import { RandomNoiseNode } from "./Nodes/RandomNoise";
 import { ImageInputNode } from "./Nodes/ImageInputNode";
-import { BlurEffectNode } from "./Nodes/BlurEffectNode";
 import { GrayscaleEffectNode } from "./Nodes/GrayscaleEffectNode";
 import { NegativeEffectNode } from "./Nodes/NegativeEffectNode";
 import { ContrastEffectNode } from "./Nodes/ContrastEffectNode";
@@ -27,7 +26,6 @@ export default class NodeRegister {
     this.LiteGraph.registerNodeType("noise/RandomNoise", RandomNoiseNode);
     this.LiteGraph.registerNodeType("input/ImageInput", ImageInputNode);
     this.LiteGraph.registerNodeType("input/WebcamInput", WebcamInputNode);
-    this.LiteGraph.registerNodeType("effect/BlurEffect", BlurEffectNode);
     this.LiteGraph.registerNodeType("effect/Grayscale", GrayscaleEffectNode);
     this.LiteGraph.registerNodeType("effect/NegativeEffect", NegativeEffectNode);
     this.LiteGraph.registerNodeType("effect/ContrastEffect", ContrastEffectNode);
